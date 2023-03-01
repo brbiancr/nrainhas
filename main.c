9981b56c2f22e83031a4779975e7dd0040715e4f
brbiancr <brbiancr@gmail.com>
2023-03-01 13:55:14 -0300

    Criando a função alocaMemoria() e alocando memória para populacaoAtual e proximaPopulacao

--- main.c
+++ main.c
@@ -1,6 +1,43 @@
 #include <stdio.h>
 
-int main(){
+// Defines
+#define TAMANHOPOPULACAO 10
+#define TAMANHOTABULEIRO 8
 
+// Variáveis globais
+int **populacaoAtual;   
+int **proximaPopulacao;
+
+// Funções
+void alocaMemoria();
+
+int main(){
+    alocaMemoria();
 return 0;
 }
+
+/*
+    ---------------
+    alocaMemoria()
+    ---------------
+
+    Aloca memória de forma dinamica para:
+    - populacaoAtual
+    - proximaPopulacao
+    - fitnessDaPopulacao
+    - individuosTorneio
+    - pais
+    - fitnessTorneio
+*/
+void alocaMemoria(){
+    int i;
+
+    populacaoAtual = (int**) malloc(sizeof(int*) * TAMANHOPOPULACAO);
+    proximaPopulacao = (int**) malloc(sizeof(int*) * TAMANHOPOPULACAO);
+
+    for (i=0; i<TAMANHOPOPULACAO; i++){
+        populacaoAtual[i] = (int*) malloc(sizeof(int) * TAMANHOTABULEIRO);
+        proximaPopulacao[i] = (int*) malloc(sizeof(int) * TAMANHOTABULEIRO);
+    }
+}
+
